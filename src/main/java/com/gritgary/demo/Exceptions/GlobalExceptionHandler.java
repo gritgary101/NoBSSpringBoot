0302b21387a8f357f52d4af3ec30ee7330699d86
Gary Zhang <gary@moocads.com>
2024-06-10 19:38:38 -0400

    fix GlobalExceptionHandler typo

--- src/main/java/com/gritgary/demo/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/gritgary/demo/Exceptions/GlobalExceptionHandler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(ProductNotFoundException.class)
+    @ExceptionHandler(CustomBaseException.class)
     public ResponseEntity<SimpleResponse> handlerProductNotFoundException(CustomBaseException exception){
         return ResponseEntity.status(exception.getStatus()).body(exception.getSimpleResponse());
     }
